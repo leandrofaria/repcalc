848b5093542d110f83ac114c3cc574dcfd589068
Leandro Faria <leandro@leandrofaria.com>
2022-12-18 18:11:21 -0300

    Fix scrolling and presentation for Sobre page

--- pages/sobre.js
+++ pages/sobre.js
@@ -20,22 +20,21 @@ const Sobre = () => {
             trabalhadas quando do uso de relógio eletrônico de ponto (REP). A
             primeira versão do sistema era baseada em linha de comando e evoluiu
             para uma versão web para facilitar o uso em diferentes dispositivos.
-            O código da aplicação encontra-se disponível em{" "}
-            <a href="https://github.com/leandrofaria/repcalc" target="_blank">
-              https://github.com/leandrofaria/repcalc
-            </a>
-            .
-          </span>
-          <p>Para maiores informações e contato, seguem links do dev:</p>
-          <div className={classes.techDisplay}>
-            <a href="https://github.com/leandrofaria" target="_blank">
-              <strong>GitHub</strong>
-            </a>
+            <br />
+            <br />
+            Para maiores informações, contato ou código fonte da aplicação,
+            visite o{" "}
             <a href="https://www.linkedin.com/in/farialaf/" target="_blank">
               <strong>LinkedIn</strong>
-            </a>
-          </div>
-
+            </a>{" "}
+            e/ou o{" "}
+            <a href="https://github.com/leandrofaria" target="_blank">
+              <strong>GitHub</strong>
+            </a>{" "}
+            do autor.
+          </span>
+          <br />
+          <br />
           <p>
             <strong>Tecnologias:</strong>
           </p>
@@ -43,9 +42,6 @@ const Sobre = () => {
           <div className={classes.techDisplay}>
             <a href="https://reactjs.org/" target="_blank">
               <div>
-                <p>
-                  <strong>React</strong>
-                </p>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/270px-React-icon.svg.png"
                   alt="React"
@@ -54,9 +50,6 @@ const Sobre = () => {
             </a>
             <a href="https://nextjs.org/" target="_blank">
               <div>
-                <p>
-                  <strong>NextJS</strong>
-                </p>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/207px-Nextjs-logo.svg.png"
                   alt="NextJS"
@@ -65,9 +58,6 @@ const Sobre = () => {
             </a>
             <a href="https://nodejs.org/" target="_blank">
               <div>
-                <p>
-                  <strong>NodeJS</strong>
-                </p>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/320px-Node.js_logo.svg.png"
                   alt="NodeJS"
@@ -82,9 +72,7 @@ const Sobre = () => {
           <div className={classes.infraDisplay}>
             <a href="https://www.interserver.net/r/480102" target="_blank">
               <div>
-                <p>
-                  <strong>InterServer</strong>
-                </p>
+                <br />
                 <img
                   src="https://www.interserver.net/logos/12946819.gif"
                   alt="InterServer"
