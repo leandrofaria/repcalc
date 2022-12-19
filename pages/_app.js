49c2b4335119716def60e05a09eda508c3d7f235
Leandro Faria <leandro@leandrofaria.com>
2022-12-19 20:31:35 -0300

    Add Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as gtag from "../lib/gtag";
+import Analytics from "../components/Analytics";
+
 import LayoutWrapper from "../components/layout/layoutWrapper";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <LayoutWrapper>
       <Component {...pageProps} />
+      <Analytics />
     </LayoutWrapper>
   );
 }
